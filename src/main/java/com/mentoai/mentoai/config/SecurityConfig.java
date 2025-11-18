6847539aa102df93cc614372efdd473b4ef39c6c
Kyengjin Kim <klo090866@gmail.com>
2025-11-18 12:33:41 +0900

    2

--- src/main/java/com/mentoai/mentoai/config/SecurityConfig.java
+++ src/main/java/com/mentoai/mentoai/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> {})
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
