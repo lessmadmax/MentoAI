fd30b189693e5ff8b1bd1565d2bcea51c2c16bc2
Kyengjin Kim <klo090866@gmail.com>
2025-11-24 21:18:04 +0900

    commit

--- src/main/java/com/mentoai/mentoai/controller/MetaDataController.java
+++ src/main/java/com/mentoai/mentoai/controller/MetaDataController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/meta/data")
+@RequestMapping("/meta")
 @RequiredArgsConstructor
 public class MetaDataController {
 
