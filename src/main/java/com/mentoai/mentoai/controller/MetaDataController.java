5aa8ee6ae95117d476a61eb54b85a36cff0cbec9
2022110204@khu.ac.kr <2022110204@khu.ac.kr>
2025-12-02 19:46:38 +0900

    fix: 고용24 직업정보 API CSV파일로 대체

--- src/main/java/com/mentoai/mentoai/controller/MetaDataController.java
+++ src/main/java/com/mentoai/mentoai/controller/MetaDataController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/meta")
+@RequestMapping("/meta/data")
 @RequiredArgsConstructor
 public class MetaDataController {
 
